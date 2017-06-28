83e7953ff8b3fcd85894884fa871f48419252cac
Fan <fancarter007@163.com>
2017-06-28 16:55:36 +0800

    route

--- index.js
+++ index.js
@@ -1,44 +1,70 @@
-const koa = require('koa')
-const logger = require('koa-logger')
-const route = require('koa-route')
-const views = require('co-views')
-const serve = require('koa-static')
+const koa = require('koa');
+const logger = require('koa-logger');
+const route = require('koa-route');
+const views = require('co-views');
+const serve = require('koa-static');
 
-const app = koa()
-app.use(logger())
+const app = koa();
+app.use(logger());
 
-app.listen(2333)
+app.listen(2333);
 
-console.log('listening 2333')
+console.log('listening 2333');
 
-app.use(serve(__dirname +'/src'))
+app.use(serve(__dirname +'/src'));
 
 let render = views(__dirname + '/page',{
     map: {html:'mustache'}
 });
 
-//router
 
-app.use(route.get('/',index))
-app.use(route.get('/new',newImg))
+const gm = require('gm');
+const dir = __dirname + '/img/';
+
+
+const randomImg = () => dir + 'img'+ Math.ceil(Math.random()*3)+'.jpg';
+const randomNum = (width) => Math.ceil(Math.random()*Number(width) + Number(width)/2);
+
 
 function *index(){
     let data = {};
     this.body = yield render('index',data)
 }
 
-const fs = require('fs')
-const path = require('path')
-const gm = require('gm')
+function *resetImg(number){
+    this.type =  'image/jpg';
+    let inputW,inputH;
 
-let dir = __dirname + '/img/'
-
-
-
-//todo
-function *newImg(ctx){
-
-    this.body = yield gm(dir + 'banner2.jpg').crop(140,140).stream()
+    if(/\d[x]\d/.test(number)){
+        inputW = number.split('x')[0];
+        inputH = number.split('x')[1];
+        this.body = gm(randomImg()).resize(inputW,inputH,'!').stream();
+    }else if(/^\d+$/.test(number)){
+        this.body = gm(randomImg()).resize(number).stream();
+    }else{
+        this.throw('Must be number,like [300x200] or [500]', 404);
+    }
 
 }
 
+function *resetImgRandom(number){
+    this.type =  'image/jpg';
+    let inputW,inputH;
+
+    if(/\d[x]\d/.test(number)){
+        inputW = number.split('x')[0];
+        inputH = number.split('x')[1];
+        this.body = gm(randomImg()).resize(randomNum(inputW),randomNum(inputH),'!').stream();
+    }else if(/^\d+$/.test(number)){
+        this.body = gm(randomImg()).resize(randomNum(number)).stream();
+    }else{
+        this.throw('Must be number,like [300x200] or [500]', 404);
+    }
+
+}
+
+//router
+
+app.use(route.get('/',index));
+app.use(route.get('/:number',resetImg));
+app.use(route.get('/r/:number',resetImgRandom));
