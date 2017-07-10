2d821b73ffb1f22d81fa846bdc1539c4e9fde562
Fan <fancarter007@163.com>
2017-07-10 14:33:50 +0800

    change to jimp

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ let render = views(__dirname + '/page',{
 
 const fs = require('fs');
 const gm = require('gm');
-const Jimp = require('jimp');
+const jimp = require('jimp');
 const dir = __dirname + '/img/';
 const imageMagick = gm.subClass({imageMagick: true});
 
 const randomImg = () => dir + 'img'+ Math.ceil(Math.random()*9)+'.jpg';
 const randomNum = (width) => Math.ceil(Math.random()*Number(width) + Number(width)/2);
 
-
 const numRegx = function(num,cb1,cb2){
     if(/\d[x]\d/.test(num)){
-        cb1();
+
+        cb1(Number(num.split('x')[0]),Number(num.split('x')[1]));
     }else if(/^\d+$/.test(num)){
         cb2();
     }
-}
+};
 
 
 function *index(){
@@ -43,42 +43,35 @@ function *index(){
 
 function *resetImg(num){
 
-
-
-
-
     function jimpLoad(cb){
-
-        Jimp.read(randomImg()).then(function(image){
-            numRegx(num,function(){
-                let w = Number(num.split('x')[0]),
-                    h = Number(num.split('x')[1]),
-                    rW,rH;
+        jimp.read(randomImg()).then(function(image){
+            numRegx(num,
+                function(w,h){
+                let rW,rH,x = 0,y = 0;
                 if (w > h){
                     rW = w;
-                    rH = Jimp.AUTO;
+                    rH = jimp.AUTO;
+                    y = (w - h)/2;
                 }else{
-                    rW = Jimp.AUTO;
+                    rW = jimp.AUTO;
                     rH = h;
+                    x = (h - w)/2;
                 }
-                
                 image.resize(rW,rH)
-                    .crop(w,h)
-                    .quality(80)
-                    .getBuffer(Jimp.MIME_JPEG,function(err,buffer){
+                    .crop(x,y,w,h)
+                    .getBuffer(jimp.MIME_JPEG,function(err,buffer){
                         if (err) throw err;
                         cb(null,buffer);
                     })
-            },function(){
+            },
+                function(){
                 image.resize(Number(num),Number(num))
-                    .quality(80)
-                    .getBuffer(Jimp.MIME_JPEG,function(err,buffer){
+                    .getBuffer(jimp.MIME_JPEG,function(err,buffer){
                         if (err) throw err;
                         cb(null,buffer);
                     })
-            })
-
-
+            }
+            )
         }).catch(function(err){
             console.error(err)
         });
@@ -91,7 +84,46 @@ function *resetImg(num){
 
 function *resetImgRandom(num){
 
-    outputBySize(this,randomImg(),num,true)
+    function jimpLoad(cb){
+        jimp.read(randomImg()).then(function(image){
+            numRegx(num,
+                function(w,h){
+                    w = randomNum(w);
+                    h = randomNum(h);
+                    let rW,rH,x = 0,y = 0;
+                    if (w > h){
+                        rW = w;
+                        rH = jimp.AUTO;
+                        y = (w - h)/2;
+                    }else{
+                        rW = jimp.AUTO;
+                        rH = h;
+                        x = (h - w)/2;
+                    }
+
+                    image.resize(rW,rH)
+                        .crop(x,y,w,h)
+                        .getBuffer(jimp.MIME_JPEG,function(err,buffer){
+                            if (err) throw err;
+                            cb(null,buffer);
+                        })
+                },
+                function(){
+                    image.resize(randomNum(num),randomNum(num))
+                        .getBuffer(jimp.MIME_JPEG,function(err,buffer){
+                            if (err) throw err;
+                            cb(null,buffer);
+                        })
+                }
+            )
+        }).catch(function(err){
+            console.error(err)
+        });
+    }
+
+    this.type = 'image/jpeg';
+    this.body = yield jimpLoad;
+
 }
 
 
@@ -101,22 +133,41 @@ function *resetImgRandom(num){
 
 function *drawImg(color,num){
 
+
+
+
     function jimpLoad(cb){
-        Jimp.read(randomImg()).then(function(image){
-            image.resize(Number(num),Number(num))
-                .quality(80)
-                .getBuffer(Jimp.MIME_JPEG,function(err,buffer){
-                    if (err) throw err;
-                    cb(null,buffer);
+
+
+        let colorNum = Number('0x'+color+'FF');
+
+        let fontPath = colorNum > Number('0x'+808080+'FF') ?  jimp.FONT_SANS_16_BLACK: jimp.FONT_SANS_16_WHITE;
+        numRegx(num,
+            function(w,h){
+                new jimp(w,h,colorNum,function(err,image){
+                    jimp.loadFont(fontPath).then(function(font){
+                        image.print(font,w/2-32,h/2-8,w+'x'+h,w).getBuffer(jimp.MIME_JPEG,function(err,buffer){
+                            if (err) throw err;
+                            cb(null,buffer);
+                        })
+                    })
                 })
-        }).catch(function(err){
-            console.error(err)
-        });
+            },
+            function(){
+                new jimp(num-0, num-0, colorNum, function (err, image) {
+                    jimp.loadFont(fontPath).then(function(font){
+                        image.print(font,num/2-32,num/2-8,num+'x'+num,num-0).getBuffer(jimp.MIME_JPEG,function(err,buffer){
+                            if (err) throw err;
+                            cb(null,buffer);
+                        })
+                    })
+                });
+            }
+        );
+
     }
 
-
     this.type = 'image/jpeg';
-
     this.body = yield jimpLoad;
 
 }
