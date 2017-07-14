288b15b48c74a3d467d117dfee3f08f99162bcb5
Fan <fancarter007@163.com>
2017-07-14 16:57:34 +0800

    :octocat:

--- index.js
+++ index.js
@@ -15,17 +15,28 @@ let render = views(__dirname + '/page',{
     map: {html:'ejs'}
 });
 
-
-
 const fs = require('fs');
 const jimp = require('jimp');
 const dir = __dirname + '/img/';
-const imgNums = 6;
-let court = 0;
+let imgNums,court = 0,imgRoaded = [];
+
+fs.readdir(dir,(err,files) => {
+    imgNums = files.length;
+    console.log('image loading...');
+    for ( let i = 0; i< imgNums; i++ ){
+        jimp.read(dir+ files[i]).then(image => {
+            court++;
+            imgRoaded.push(image);
+            if (court === imgNums){
+                console.log('All image loaded over! And listening 2333')
+            }
+        }).catch(err => {console.error(err)})
+    }
+});
 
 const randomNum = (width) => Math.ceil(Math.random()*Number(width) + Number(width)/2);
 
-const numRegx = function(num,cb1,cb2){
+const numRegx = (num,cb1,cb2) => {
     if(/\d[x]\d/.test(num)){
         cb1(Number(num.split('x')[0]),Number(num.split('x')[1]));
     }else if(/^\d+$/.test(num)){
@@ -33,24 +44,10 @@ const numRegx = function(num,cb1,cb2){
     }
 };
 
-let imgRoaded = [];
-
-for ( let i = 1; i< imgNums+1; i++ ){
-    jimp.read(dir+'img'+i+'.jpg').then(function(image){
-        imgRoaded.push(image);
-        court++;
-        console.log('image'+i+' loaded...');
-        if (court === imgNums){
-            console.log('All image loaded over!')
-        }
-    }).catch(function(err){console.error(err)})
-}
-
 const imgRandom = () => _.cloneDeep(imgRoaded[Math.ceil(Math.random()*(imgNums-1))]);
 
 function *index(){
-    let data = {};
-    this.body = yield render('index',data)
+    this.body = yield render('index',{})
 }
 
 function imgResizeCrop(w,h,image,cb){
@@ -66,49 +63,43 @@ function imgResizeCrop(w,h,image,cb){
     }
     image.resize(rW,rH)
     .crop(x,y,w,h)
-    .getBuffer(jimp.MIME_JPEG,function(err,buffer){
+    .getBuffer(jimp.MIME_JPEG,(err,buffer) => {
         if (err) throw err;
         cb(null,buffer);
     })
 }
 
 function *resetImg(num){
-
-    function jimpLoad(cb){
-        let image = imgRandom();
-
-        numRegx(num,function(w,h){
-            imgResizeCrop(w,h,image,cb);
-        },function(){
-            image.resize(Number(num),Number(num))
-            .getBuffer(jimp.MIME_JPEG,function(err,buffer){
-                if (err) throw err;
-                cb(null,buffer);
-            })
-        })
-    }
-
     this.type = 'image/jpeg';
-    this.body = yield jimpLoad;
+    this.body = yield cb => {
+        let image = imgRandom();
+        numRegx(num,(w,h) => imgResizeCrop(w,h,image,cb)
+        ,() => {
+            image.resize(Number(num),Number(num))
+                .getBuffer(jimp.MIME_JPEG,function(err,buffer){
+                    if (err) throw err;
+                    cb(null,buffer);
+                })
+        })
+    };
 }
 
 
 function *resetImgRandom(num){
-    function jimpLoad(cb){
+    this.type = 'image/jpeg';
+    this.body = yield cb => {
         let image = imgRandom();
         numRegx(num,(w,h) => imgResizeCrop(randomNum(w),randomNum(h),image,cb),
             () => imgResizeCrop(randomNum(num),randomNum(num),image,cb)
         )
-    }
-    this.type = 'image/jpeg';
-    this.body = yield jimpLoad;
+    };
 }
 
 function drawSolidColorImg(w,h,colorNum,cb){
     let fontPath = colorNum > Number('0x'+808080+'FF') ?  jimp.FONT_SANS_16_BLACK: jimp.FONT_SANS_16_WHITE;
-    new jimp(w,h,colorNum,function(err,image){
-        jimp.loadFont(fontPath).then(function(font){
-            image.print(font,w/2-28,h/2-8,w+'x'+h,w).getBuffer(jimp.MIME_JPEG,function(err,buffer){
+    new jimp(w,h,colorNum,(err,image) => {
+        jimp.loadFont(fontPath).then(font => {
+            image.print(font,w/2-28,h/2-8,w+'x'+h,w).getBuffer(jimp.MIME_JPEG,(err,buffer) => {
                 if (err) throw err;
                 cb(null,buffer);
             })
@@ -117,40 +108,28 @@ function drawSolidColorImg(w,h,colorNum,cb){
 }
 
 function *drawImg(color,num){
-
-    function jimpLoad(cb){
+    this.type = 'image/jpeg';
+    this.body = yield cb => {
         let colorNum = Number('0x'+color+'FF');
         numRegx(num,(w,h) => drawSolidColorImg(w,h,colorNum,cb),
             () => drawSolidColorImg(num-0,num-0,colorNum,cb)
         );
-    }
-
-    this.type = 'image/jpeg';
-    this.body = yield jimpLoad;
-
+    };
 }
 
 function *drawImgRandom(color,num){
-
-    function jimpLoad(cb){
+    this.type = 'image/jpeg';
+    this.body = yield cb => {
         let colorNum = Number('0x'+color+'FF');
         numRegx(num,(w,h) => drawSolidColorImg(randomNum(w),randomNum(h),colorNum,cb),
             () => drawSolidColorImg(randomNum(num),randomNum(num),colorNum,cb)
         )
-    }
-
-    this.type = 'image/jpeg';
-    this.body = yield jimpLoad;
-    
+    };
 }
 
-
-
 //router
-
 app.use(route.get('/',index));
 app.use(route.get('/r@:color/:num',drawImgRandom));
 app.use(route.get('/@:color/:num',drawImg));
 app.use(route.get('/:num',resetImg));
 app.use(route.get('/r/:num',resetImgRandom));
-
